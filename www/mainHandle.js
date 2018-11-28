6b5a43cdcd937a512fa54292b6c1cb74e57cea57
Andrey Feldman <AndreyF@corp.supersol.co.il>
2018-11-28 12:42:39 +0200

    bug

--- www/mainHandle.js
+++ www/mainHandle.js
@@ -4,7 +4,7 @@ var NativeStorageError = require('./NativeStorageError');
 
 function isInBrowser() {
   inBrowser = (window.cordova && (window.cordova.platformId === 'browser' || window.cordova.platformId === 'osx')) || !(window.phonegap || window.cordova);
-  inBrowser = inBrowser || !device.available;
+  inBrowser = inBrowser || !device ||!device.available;
   return inBrowser;
 }
 
